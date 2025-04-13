e8569b688edede7587aaa2409fca8e69f593ff08
unknown <alinecaleffi97@gmail.com>
2025-04-13 15:54:37 -0300

    Criando e implementando interfaces

---
+++ src/app/interface/results.ts
@@ -0,0 +1,19 @@
+export interface Results {
+    id: number
+    name: string
+    status: string
+    species: string
+    type: string
+    gender: string // sexo
+    origin: any
+    location: any
+    image: string
+    episode: any
+    dimension: string
+    residents: Array<string>
+    air_date: string
+    characters: Array<string> //personagens
+    url: string
+    created: string
+  }
+  
